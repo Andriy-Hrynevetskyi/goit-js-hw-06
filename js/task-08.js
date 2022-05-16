763906ab64f89121a06f29f992cb1fb3b0f45b19
unknown <andrijgrineveckij@gmail.com>
2022-05-17 01:11:05 +0300

    task 8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const formRef = document.querySelector(".login-form");
 
+const onFormSubmit = (event) => {
+  event.preventDefault();
+
+  const { email, password } = event.currentTarget.elements;
+
+  if (email.value !== "" && password.value !== "") {
+    // const formData = new FormData(event.currentTarget);
+    // event.currentTarget.reset();
+    // return formData;
+
+    const resultObj = {
+      email: email.value,
+      password: password.value,
+    };
+
+    event.currentTarget.reset();
+
+    return console.log(resultObj);
+  }
+
+  return alert("Empty fields are forbidden!!!");
+};
+
+formRef.addEventListener("submit", onFormSubmit);

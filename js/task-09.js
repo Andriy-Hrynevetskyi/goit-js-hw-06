25d74477db2e4ad959f2804e377169ecb2b5c13a
unknown <andrijgrineveckij@gmail.com>
2022-05-17 14:15:56 +0300

    task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
+const refs = {
+  changeColorBtn: document.querySelector(".change-color"),
+  colorValue: document.querySelector(".color"),
+  body: document.querySelector("body"),
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function onBodyBgColorChange() {
+  let newColor = getRandomHexColor();
+  refs.body.style.backgroundColor = newColor;
+  refs.colorValue.textContent = `${newColor}`;
+}
+
+refs.changeColorBtn.addEventListener("click", onBodyBgColorChange);
